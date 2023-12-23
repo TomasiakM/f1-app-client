8b8862acc261d45fafb6bc8ff4a72707980fb9fa
TomasiakM <marcintomasiak00@gmail.com>
2023-12-23 22:36:57 +0100

    Add mobile nav

--- components/The/Nav.vue
+++ components/The/Nav.vue
@@ -1,57 +1,71 @@
 <template>
-  <div
-    class="sticky top-0 left-0 w-full bg-card border-b border-b-gray p-2 z-40"
-  >
-    <div class="container mx-auto">
-      <div class="flex items-center justify-between">
-        <NuxtLink to="/" class="text-xl font-semibold">
-          {{ brand }}
-        </NuxtLink>
-        <div class="hidden md:flex items-center gap-2">
-          <NuxtLink to="/klasyfikacja" class="nav-link">Klasyfikacja</NuxtLink>
-          <NuxtLink to="/kalendarz" class="nav-link">Kalendarz</NuxtLink>
-          <NuxtLink to="/kierowcy" class="nav-link">Kierowcy</NuxtLink>
-          <NuxtLink to="/zespoly" class="nav-link">Zespoły</NuxtLink>
-          <NuxtLink to="/tory" class="nav-link">Tory</NuxtLink>
-          <NuxtLink
-            v-if="userStore.user?.isAdmin"
-            to="/panel"
-            class="bg-primary hover:bg-primary-hover text-white duration-200 transition-colors rounded px-2"
-          >
-            Panel
+  <div>
+    <div
+      class="sticky top-0 left-0 w-full bg-card border-b border-b-gray p-2 z-40"
+    >
+      <div class="container mx-auto">
+        <div class="flex items-center justify-between">
+          <NuxtLink to="/" class="text-xl font-semibold">
+            {{ brand }}
           </NuxtLink>
 
-          <div class="flex items-center pl-4 border-l border-l-gray">
-            <div v-if="userStore.isAuthorized" class="flex items-center gap-1">
-              <div class="">
-                Witaj,
-                <span class="font-semibold">
-                  {{ userStore.getUser?.username }}
-                </span>
-              </div>
+          <button @click="isOpen = !isOpen" class="md:hidden">
+            <SvgHamburger class="w-6 h-6" />
+          </button>
 
-              <button @click="userStore.logout()">
-                <SvgLogout class="!w-5 !h-5" />
-              </button>
-            </div>
-            <div v-else class="flex items-center gap-1">
-              <NuxtLink
-                to="/logowanie"
-                class="nav-button-link bg-primary hover:bg-primary-hover text-white"
+          <div class="hidden md:flex items-center gap-2">
+            <NuxtLink to="/klasyfikacja" class="nav-link">
+              Klasyfikacja
+            </NuxtLink>
+            <NuxtLink to="/kalendarz" class="nav-link">Kalendarz</NuxtLink>
+            <NuxtLink to="/kierowcy" class="nav-link">Kierowcy</NuxtLink>
+            <NuxtLink to="/zespoly" class="nav-link">Zespoły</NuxtLink>
+            <NuxtLink to="/tory" class="nav-link">Tory</NuxtLink>
+            <NuxtLink
+              v-if="userStore.user?.isAdmin"
+              to="/panel"
+              class="bg-primary hover:bg-primary-hover text-white duration-200 transition-colors rounded px-2"
+            >
+              Panel
+            </NuxtLink>
+
+            <div class="flex items-center pl-4 border-l border-l-gray">
+              <div
+                v-if="userStore.isAuthorized"
+                class="flex items-center gap-1"
               >
-                Logowanie
-              </NuxtLink>
-              <NuxtLink
-                to="/rejestracja"
-                class="nav-button-link bg-secondary hover:bg-secondary-hover text-white"
-              >
-                Rejestracja
-              </NuxtLink>
+                <div class="font-semibold">
+                  {{ userStore.getUser?.username }}
+                </div>
+
+                <button @click="userStore.logout()">
+                  <SvgLogout class="w-5 h-5" />
+                </button>
+              </div>
+              <div v-else class="flex items-center gap-1">
+                <NuxtLink
+                  to="/logowanie"
+                  class="nav-button-link bg-primary hover:bg-primary-hover text-white"
+                >
+                  Logowanie
+                </NuxtLink>
+                <NuxtLink
+                  to="/rejestracja"
+                  class="nav-button-link bg-secondary hover:bg-secondary-hover text-white"
+                >
+                  Rejestracja
+                </NuxtLink>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <TheMobileNav
+      v-if="isOpen"
+      class="md:hidden z-40"
+      @close="isOpen = false"
+    />
   </div>
 </template>
 
@@ -62,6 +76,8 @@ const config = useRuntimeConfig();
 const userStore = useUserStore();
 
 const { brand } = config.public;
+
+const isOpen = ref(false);
 </script>
 
 <style lang="postcss" scoped>
